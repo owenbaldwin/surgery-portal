3f4203b4affb31e176d2b7d67d87f03bc22cc347
Owen Baldwin <owenabaldwin@gmail.com>
2022-04-05 15:55:44 +0100

    simple calendar added and consultation Index view created and working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def create
+    if @user.save
+      redirect_to user_path(current_user)
+    end
+    # REDIRECT TO PROFILE
+  end
+
+
+
+
 end
